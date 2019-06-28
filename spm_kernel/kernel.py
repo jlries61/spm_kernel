edb2ceb83c4ac16c6f92b7aaf20ad1f8f6d720d4
John L. Ries <jries@salford-systems.com>
2019-06-27 17:36:03 -0700

    Revert "Amendment to kernel_json"
    This reverts commit 6b24b6677792e4d666493c64fba9f61db8170d42.
    
    And fixes the invocation of the python exec name.

--- spm_kernel/kernel.py
+++ spm_kernel/kernel.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this module.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 from IPython.display import Image, SVG
 from IPython.display import display, HTML
 from metakernel import MetaKernel, ProcessMetaKernel, pexpect, u
@@ -59,7 +60,7 @@ class SPMKernel(ProcessMetaKernel):
                    'file_extension': '.cmd'}
   # The following is installed into Jupyter as SPM/kernel.json
   # The name element must be defined in order for the installer to work correctly
-  kernel_json = {"argv": [sys_executable, "-m", "spm_kernel",
+  kernel_json = {"argv": [sys.executable, "-m", "spm_kernel",
                           "-f", "{connection_file}"],
                  "display_name": "SPM",
                  "language": "SPM"}
