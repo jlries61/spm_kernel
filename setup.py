2824829f4e2527cd07d6bdb399a9b6c2324ec0f1
John L. Ries <john@theyarnbard.com>
2026-01-21 08:38:36 -0800

    Update setup.py
    Correct name of SPM

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with io.open("spm_kernel/version.py", encoding='utf-8') as fid:
 setup(
   name = "spm_kernel",
   version = __version__,
-  description = "Jupyter kernel for Salford Predictive Miner (SPM)",
+  description = "Jupyter kernel for Salford Predictive Modeler (SPM)",
   url="https://github.com/jlries61/spm_kernel",
   author="John L. Ries",
   author_email="john@theyarnbard.com",
